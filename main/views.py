a9a29f657f2f4c635722d986531db9baa3c72a02
Daniil Semenov <daniils2648@gmail.com>
2022-06-19 21:44:16 +0300

    added new: image, time selection

--- main/views.py
+++ main/views.py
@@ -30,29 +30,49 @@ def api_request(request):
 
 
 def context_creator(request, response):
+
     data = response.json()
     data_list = data['list']
+
+    try:
+        user_time = int(request.POST.get('user-time'))
+    except TypeError:
+        user_time = 0
+
+    times = {}
+    for x in range(len(data_list)):
+        time = datetime.datetime.utcfromtimestamp(data_list[x]['dt'])
+        times[x] = time
 
     city = data['city']
     city_name = city['name']
     city_country = city['country']
-    sunrise = city['sunrise']
-    sunset = city['sunset']
+
+    sunrise_seconds = city['sunrise'] + city['timezone']
+    sunset_seconds = city['sunset'] + city['timezone']
+    sunrise_date = datetime.datetime.utcfromtimestamp(sunrise_seconds)
+    sunset_date = datetime.datetime.utcfromtimestamp(sunset_seconds)
+    sunrise = datetime.datetime.strftime(sunrise_date, '%H:%M')
+    sunset = datetime.datetime.strftime(sunset_date, '%H:%M')
+    day_length = sunset_date - sunrise_date
 
     city_timezone = city['timezone']
     local_time = datetime.datetime.now()
     time = datetime.timedelta(seconds=city_timezone - 10800) + local_time
     current_time = datetime.datetime.strftime(time, '%H:%M')
 
-    for x in range(1):
-        temp_data = data_list[x]['main']
-        temp = f'{temp_data["temp"]:.0f}'
-        feels_like = temp_data['feels_like']
-        weather = data_list[x]['weather'][0]['description'].title()
-        clouds = data_list[x]['clouds']
-        wind = data_list[x]['wind']['speed']
-        humidity = temp_data['humidity']
-        pressure = temp_data['pressure']
+    temp_data = data_list[user_time]['main']
+    temp = f"{temp_data['temp']:.0f}"
+    feels_like = f"{temp_data['feels_like']:.0f}"
+    weather = data_list[user_time]['weather'][0]['description'].title()
+    clouds = data_list[user_time]['clouds']['all']
+    wind = data_list[user_time]['wind']['speed']
+    humidity = temp_data['humidity']
+    pressure = temp_data['pressure']
+
+    dt = data_list[user_time]['dt']
+    weather_time = datetime.datetime.utcfromtimestamp(dt)
+    weather_time_str = datetime.datetime.strftime(weather_time, '%H:%M')
 
     context = {
         'city_name': city_name,
@@ -60,6 +80,7 @@ def context_creator(request, response):
         'time': current_time,
         'sunrise': sunrise,
         'sunset': sunset,
+        'day_length': day_length,
         'temp': temp,
         'feels_like': feels_like,
         'weather': weather,
@@ -67,6 +88,8 @@ def context_creator(request, response):
         'wind': wind,
         'humidity': humidity,
         'pressure': pressure,
+        'weather_time': weather_time_str,
+        'times': times,
     }
 
     return context
